e8b29bfd089756ac788dbc1361d88e4ceca7f0f2
Anthony Moss <asmoss93@gmail.com>
2019-07-10 23:57:00 -0400

    adds component that will be the base of our calculator itself, it will hold the buttons, output field etc

---
+++ src/components/CalculatorBase.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class CalculatorBase extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <div> 
+                <p>to be a calculator</p>
+            </div>
+        )}
+}
+
+export default CalculatorBase
