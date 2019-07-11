6b0a460780d1f3dc5880de1bef352a194fb73a85
Anthony Moss <asmoss93@gmail.com>
2019-07-11 16:59:09 -0400

    adds state so calc can work,and updates number buttons with functionality, they add to a number when pressed, right now when num button pressed, that number is added to the current tracked number.

--- src/components/CalculatorBase.js
+++ src/components/CalculatorBase.js
@@ -4,38 +4,65 @@ import React from 'react';
 class CalculatorBase extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            // startingOutput: 0,
+            numsToUse: [],
+            currentNumber: '0',
+            answer: 0,
+            output: 0
+        }
     }
     render() {
         return (
             <div>
                 <div>
-                    <p>to be output</p>
+                    <text name='num1'>{this.state.currentNumber}</text>
+                    {/* <text name='output' value={this.state.output}>{this.state.output}</text> */}
                 </div>
                 <div>
-                    <button>C</button>
-                    <button>+/-</button>
-                    <button>%</button>
-                    <button>/</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>X</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>-</button>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>+</button>
-                    <button>0</button>
-                    <button>.</button>
-                    <button>=</button>
+                    <div className='modifiers'>
+                        <button>C</button>
+                        <button>+/-</button>
+                        <button>%</button>
+                        <button>=</button>
+                    </div>
+                    <div className='operations'>
+                        <button>+</button>
+                        <button>-</button>
+                        <button>X</button>
+                        <button>/</button>
+                    </div>
+                    <div className='numbers'>
+                        <button value ='7' onClick={this.addNumber}>7</button>
+                        <button value= '8' onClick={this.addNumber}>8</button>
+                        <button value ='9' onClick={this.addNumber}>9</button>
+                        <button value ='4' onClick={this.addNumber}>4</button>
+                        <button value ='5' onClick={this.addNumber}>5</button>
+                        <button value ='6' onClick={this.addNumber}>6</button>
+                        <button value ='1' onClick={this.addNumber}>1</button>
+                        <button value ='2' onClick={this.addNumber}>2</button>
+                        <button value ='3' onClick={this.addNumber}>3</button>
+                        <button value ='0' onClick={this.addNumber}>0</button>
+                        <button>.</button>
+                    </div>
                 </div>
-
             </div>
         )}
+
+    handleOutput = (event) => {
+        this.setState({[event.target.name]:event.target.value})
+    }
+
+    addNumber = (event) => {
+        let newNum = ''
+        if (this.state.currentNumber === '0')  {
+            newNum = event.target.value
+        } else {
+            newNum = this.state.currentNumber + event.target.value
+        }
+        this.setState({currentNumber: newNum})
+    }
+
 }
 
 export default CalculatorBase
