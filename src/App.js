28172631ca481660c477580bbea8b95f7b260baa
Anthony Moss <asmoss93@gmail.com>
2019-07-10 23:56:06 -0400

    removes unneeded stuff from create react app, adds just simple div for now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CalculatorBase from './components/CalculatorBase';
 
 function App() {
   return (
@@ -8,16 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Clock
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CalculatorBase />
       </header>
     </div>
   );
