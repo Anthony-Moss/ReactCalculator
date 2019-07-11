08a7ac321c3d10c0377a728ce9d20a70696aaff4
Anthony Moss <asmoss93@gmail.com>
2019-07-11 00:48:13 -0400

    adds placeholder for output field and adds all base buttons the calculator will need

--- src/components/CalculatorBase.js
+++ src/components/CalculatorBase.js
@@ -8,8 +8,32 @@ class CalculatorBase extends React.Component {
     }
     render() {
         return (
-            <div> 
-                <p>to be a calculator</p>
+            <div>
+                <div>
+                    <p>to be output</p>
+                </div>
+                <div>
+                    <button>C</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                    <button>/</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>X</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>-</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                    <button>0</button>
+                    <button>.</button>
+                    <button>=</button>
+                </div>
+
             </div>
         )}
 }
